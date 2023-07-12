07300ab1693a68302ade6e9f9bb98d3bb4ead4dd
Dinesh-7075 <jammidinesh717@gmail.com>
2023-07-12 19:44:57 +0530

    updated Search functiom

--- script.js
+++ script.js
@@ -16,16 +16,15 @@ async function fetchPincodeDetails() {
     try {
       const response = await fetch(endpoint, { method: "GET" });
       postalCodeData = await response.json();
-      console.log( `Postal data fetched for ${pincode}`);
+      console.log(`Postal data fetched for ${pincode}`);
       console.log(postalCodeData);
       renderTable(postalCodeData);
       nullPincode.style.visibility = "hidden";
       SearchFilter.style.visibility = "visible";
-
     } catch {
       nullPincode.style.visibility = "visible";
       SearchFilter.style.visibility = "hidden";
-      nullPincode.innerHTML = "Please enter your Six digit Postal code......";    
+      nullPincode.innerHTML = "Please enter your Six digit Postal code......";
     }
   }
 }
@@ -35,6 +34,7 @@ function renderTable(data) {
   const tableBody = document.getElementById("tableBody");
   tableBody.innerHTML = "";
   data.forEach((item) => {
+    //  console.log(item);
     item.PostOffice.forEach((loc) => {
       const row = document.createElement("tr");
       row.innerHTML = `
@@ -50,17 +50,23 @@ function renderTable(data) {
   });
 }
 
-  //search function
-  document.getElementById('SearchFilter').addEventListener('keyup', ()=>{
-    const searchTerm=document.getElementById('SearchFilter').value.trim().toLowerCase();
-    if(searchTerm==''){
-        renderTable(postalCodeData);
-        return;
-    }
-    const filteredData = postalCodeData[0].PostOffice.filter(function (item){
-            return item.Name.includes(searchTerm);
-        });
-        
-            renderTable(filteredData);
-}); 
-    
+//search function
+const searchInput = document.getElementById("SearchFilter");
+searchInput.addEventListener("keyup", () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm == "") {
+    renderTable(postalCodeData);
+    return;
+  }
+  console.log(postalCodeData, "hello");
+  const filteredStudents = postalCodeData[0].PostOffice.filter(function (item) {
+    return item.Name.toLowerCase().includes(searchTerm);
+  });
+  console.log(filteredStudents);
+  const newData = [
+    {
+      PostOffice: filteredStudents,
+    },
+  ];
+  renderTable(newData);
+})
